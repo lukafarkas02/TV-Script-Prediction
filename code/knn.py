4be4163811c4bec693123dc7d47b3cbf8577d6a3
Luka Farkaš <105086132+lukafarkas02@users.noreply.github.com>
2024-06-29 05:24:05 +0200

    frequencies of all grams by category

--- code/knn.py
+++ code/knn.py
@@ -10,6 +10,7 @@ import pandas as pd
 from sklearn.metrics import accuracy_score
 import torch
 from sklearn.model_selection import train_test_split
+from collections import Counter
 
 
 def load_scripts(path):
@@ -31,11 +32,57 @@ def preprocessing_data(text):
     return preprocessed_text
 
 
-def get_n_grams_frequencies(X, y, n):
+def get_n_grams_frequencies(texts, categories, n):
     total_grams = 0
-    # for
+    n_gram_category = {}
+    n_gram_counter = {}
+    for text, category in zip(texts, categories):
+        total_line_length = len(text)
+        line_length = total_line_length - n + 1
+        for line in range(line_length):
+            n_gram_word = text[line:n + line]
+            total_grams += 1
+            if n_gram_word+"@"+category in n_gram_category:
+                n_gram_category[n_gram_word+"@"+category] += 1
+            else:
+                n_gram_category[n_gram_word+"@"+category] = 1
+            if n_gram_word in n_gram_counter:
+                n_gram_counter[n_gram_word] += 1
+            else:
+                n_gram_counter[n_gram_word] = 1
 
-    return [None]
+    counter = Counter(n_gram_counter)
+    keys_sorted_by_value = sorted(counter.keys(), key=lambda k: counter[k], reverse=True)
+
+    unique_categories = categories.unique()
+    frequency_dict = {}
+    for n_gram_sorted in keys_sorted_by_value:
+        for category in unique_categories:
+            key = n_gram_sorted + "@" + category
+            if key in n_gram_category.keys():
+                frequency_dict[key] = n_gram_category[key] / total_grams
+                # print(frequency_dict[key])
+            else:
+                frequency_dict[key] = 0
+
+    sorted_frequency = sorted(frequency_dict.items(), key=lambda x: x[1], reverse=True)
+    # for idx, (word, freq) in enumerate(sorted_frequency[:100], start=1):
+    #     print(f"{idx}. {word}: {freq}")
+    counter = 0
+    zero_counter = 0
+    non_zero_counter = 0
+    for key, value in sorted_frequency:
+        # print(value)
+        if value == 0:
+            zero_counter += 1
+        else:
+            non_zero_counter += 1
+
+    print("Non zero: " + str(non_zero_counter))
+    print("Zero counter: " + str(zero_counter))
+
+    return sorted_frequency
+
 
 
 
@@ -55,26 +102,27 @@ if __name__ == "__main__":
     df['text'] = df['text'].astype(str)
     df['text'] = df['text'].apply(preprocessing_data)
 
-    X = df[['text']]
-    print(df['text'])
-    print("--------")
-    print(df[['text']])
-    input()
+    X = df['text']
+    # print(df['text'])
+    # print("--------")
+    # print(df[['text']])
+    # input()
     y = df['category']
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     frequencies = get_n_grams_frequencies(X_train, y_train, 3)
+    print(frequencies)
 
-    knn = KNNClassifier(k=4000)
-    knn.fit(frequencies, y_train)
+    # knn = KNNClassifier(k=4000)
+    # knn.fit(frequencies, y_train)
 
     # x_test = test_data_preprocessing(X_test, 3)
     # print(x_test)
     # y_pred = knn.predict(x_test)
     # accuracy = (y_pred == y_test_tensor).sum().item() / len(y_test_tensor)
     # print("Tačnost:", accuracy)
-    accuracy = accuracy_score(y_test, y_pred)
-    print("Accuracy:", accuracy * 100)
-    print(datetime.datetime.now())
+    # accuracy = accuracy_score(y_test, y_pred)
+    # print("Accuracy:", accuracy * 100)
+    # print(datetime.datetime.now())
 
 
